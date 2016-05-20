38b99b0defea7b30e7f3dee23ec41e3797a74aa9
PETIT Antoine <antoinepetit.16@gmail.com>
2016-05-21 00:37:17 +0200

    validCo

--- validationConnexion.php
+++ validationConnexion.php
@@ -1,3 +1,23 @@
 <?php
-
+include_once("model/sessions.php");
+include_once("model/membres.php");
+if(!isset($_POST['Email']) or !isExistingPseudo($_POST['Email']))
+	Header('Location: connexion.php?err=1');
+else
+{
+	$user = getInfosUsersByEmail($_POST['Email']);
+	$user['NomMembre'] = htmlspecialchars($user['NomMembre']);
+	$user['PrenomMembre'] = htmlspecialchars($user['PrenomMembre']);
+	$user['IdMembre'] = htmlspecialchars($user['IdMembre']);
+	$user['Admin'] = htmlspecialchars($user['Admin']);
+	if(sha1($_POST['Mdp']) != $user['Mdp'])
+		Header('Location: connexion.php?err=2');
+	else
+	{
+		$_SESSION['session_id'] = $user['IdMembre'];
+		$_SESSION['session_password'] = $user['Mdp'];
+		$page_title = 'Bonjour ' .$user['PrenomMembre']. ' ' . $user['NomMembre'] . ' !';
+		include_once("view/validerConnexion.php");
+	}
+}
 
