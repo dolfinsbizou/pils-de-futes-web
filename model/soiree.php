131e83ed82d4ef8317a23c38e5654b1cb20f3b3e
PETIT Antoine <antoinepetit.16@gmail.com>
2016-05-21 01:33:20 +0200

    controllers plusieur pages

--- model/soiree.php
+++ model/soiree.php
@@ -7,6 +7,7 @@ function getSoireeVote()
 	global $bdd;
 
 	$answer = $bdd->prepare("SELECT IdSoiree,NomSoiree,DateSoiree FROM Soiree WHERE DateFermetureVote > now()");
+	$answer->execute([]);
 	$data = $answer->fetchAll(PDO::FETCH_ASSOC);
 	return($data);	
 }
@@ -16,6 +17,17 @@ function getSoiree()
 	global $bdd;
 
 	$answer = $bdd->prepare("SELECT IdSoiree,NomSoiree,DateSoiree FROM Soiree WHERE DateFermetureVote < now() and DateSoiree > now()");
+	$answer->execute([]);
+	$data = $answer->fetchAll(PDO::FETCH_ASSOC);
+	return($data);
+}
+//get info soiree idSoiree
+function getSoireeInfoById($idSoiree,$idMembre)
+{
+	global $bdd;
+
+	$answer = $bdd->prepare("SELECT NomSoiree,DateSoiree FROM Soiree WHERE IdSoiree = ? AND IdMembre = ?");
+	$answer->execute(array($idSoiree,$idMembre));
 	$data = $answer->fetchAll(PDO::FETCH_ASSOC);
 	return($data);
 }
--- profil.php
+++ profil.php
@@ -1,3 +1,19 @@
 <?php
-$page_title = "profil";
+include_once("model/sessions.php");
+include_once("model/membres.php");
+if(isLogged())
+{
+	$data = getUserInfoById(getUserId());
+	$uid = getUserId();
+}else {
+	header('Location: ./');
+	exit;
+}
+
+$data['NomMembre'] = htmlspecialchars($data['NomMembre']);
+$data['PrenomMembre'] = htmlspecialchars($data['PrenomMembre']);
+$data['Email'] = htmlentities($data['Email']);
+$data['Admin'] = htmlspecialchars($data['Admin']);
+
+$page_title = 'Profil de '.$data['PrenomMembre'].' ' . $data['NomMembre'];
 include_once("view/profil.php");
