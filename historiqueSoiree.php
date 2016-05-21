6d3e8af8090e3e651234f826378688124ca1c82a
dolfinsbizou <jorandon@gmail.com>
2016-05-21 03:23:05 +0200

    contrôleur historique

--- historiqueSoiree.php
+++ historiqueSoiree.php
@@ -2,5 +2,18 @@
 include_once("model/sessions.php");
 include_once("model/soiree.php");
 
+if(!isLogged())
+{
+	Header('Location: ./connexion.php');
+	exit(0);
+}
+
+$historique = getHistSoiree();
+
+foreach($historique as &$hist)
+{
+	$hist['NomSoiree'] = htmlspecialchars($hist['NomSoiree']);
+}
+
 $page_title = "Historique des soirées";
 include_once("view/historiqueSoiree.php");
