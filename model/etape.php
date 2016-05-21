cfdbef78b73a988a2bc298a0b3ca205a1f143c85
Theo GUIDET <theo.guidet@hotmail.fr>
2016-05-21 03:28:09 +0200

    requete select etape

---
+++ model/etape.php
@@ -0,0 +1,12 @@
+<?php
+
+function getEtape($idEtape, $idConfig)
+{
+	global $bdd;
+	
+	$answer = $bdd->prepare("SELECT HeureDebut, HeureFin, IdLieu, Type FROM Etape WHERE IdEtape = ? AND IdConfig = ?")
+	$data = $answer->execute(array($idEtape, $idConfig));
+	$data = $answer->fetch(PDO::FETCH_ASSOC);
+	return($data);
+}
+?>
