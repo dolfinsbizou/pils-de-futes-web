346fd765fa55253b08f6a9becc7fa124e984f98d
dolfinsbizou <jorandon@gmail.com>
2016-05-21 04:48:15 +0200

    t

--- ajouterConfig.php
+++ ajouterConfig.php
@@ -5,25 +5,13 @@
 	if (!isLogged())
 		Header("Location: ./");
 
-	else if (!isAdmin($_SESSION['session_id']))
+	else if (!isAdmin(getUserId()))
 		Header("Location: ./");
 
 	else 
-	{
-		if(isset($_GET['err']))
-		{
-			$err = "<div id=\"err\" class=\"icon\">";
-			switch($_GET['err'])
-			{
-				case 1:
-					$err.= "Merci de compléter tous les champs du formulaire !";
-				default:
-					$err.= "Cessez donc de modifier l'URL, petit malandrin !";
-			}
-			$err.= "</div>";
-		}
-		else
-			$err='';
+	{	
+
+
 
 		$page_title = "ajouterConfig";
 		include_once("view/ajouterConfig.php");
