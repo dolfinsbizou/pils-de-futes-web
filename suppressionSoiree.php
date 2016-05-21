2eb0283ed3bd1e890ea2d7695c6874b1c0293b5c
Theo GUIDET <theo.guidet@hotmail.fr>
2016-05-21 02:31:43 +0200

    Suppression Soiree

--- suppressionSoiree.php
+++ suppressionSoiree.php
@@ -1,3 +1,20 @@
 <?php
 $page_title = "suppressionSoiree";
+include_once("model/sessions.php");
 include_once("view/suppressionSoiree.php");
+
+function supprimerSoiree($IdSoiree)
+{
+	if(isAdmin())
+	{
+		global $bdd;
+
+		$answer = $bdd->prepare("DELETE FROM Soiree WHERE IdSoiree = ?");
+		$answer->execute([$IdSoiree]);
+	}
+	else
+	{
+		
+	}
+	
+}
