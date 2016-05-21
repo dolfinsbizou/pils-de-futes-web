3b9cfeb489ab80e091018c83dd7fb10b06551cbb
V1nkey <kevin.chidiac@gmail.com>
2016-05-21 05:18:59 +0200

    ValideVote.php

--- valideVote.php
+++ valideVote.php
@@ -1,12 +1,12 @@
 <?php
 
-	include_once($PROJECT_ROOT . "model/connectBdd.php");
+	include_once("model/sessions");
 	include_once("model/vote.php");
 
 
 	if (!empty($_POST))
 		if (!aVote(getUserID(), $_POST["idConfig"]))
-			addVote($_POST["idSoiree"], $_POST["choixVote"]);
+			addVote($_POST["choixVote"], getUserID());
 
 	Header('Location: soiree.php?id='.$_POST["idSoiree"]);
 
