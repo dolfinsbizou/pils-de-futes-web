8b3baac243b8392bd135b62bf501764f6e7ec82b
dolfinsbizou <jorandon@gmail.com>
2016-05-21 04:41:16 +0200

    correction

--- ajouterSoiree.php
+++ ajouterSoiree.php
@@ -1,3 +1,20 @@
 <?php
+	include_once("model/sessions.php");
+	include_once("model/membres.php");
+	include_once("model/soiree.php");
+	
+	if (!isLogged())
+	{
+		Header("Location: ./");
+		exit(0);
+	}
+
+	else if (!isAdmin(getUserID()))
+	{
+		Header("Location: ./");
+		exit(0);
+	}
+	$user = getInfosUsersById($_SESSION['session_id']);
+
 $page_title = "ajouterSoiree";
 include_once("view/ajouterSoiree.php");
